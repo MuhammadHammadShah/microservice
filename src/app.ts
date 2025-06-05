b397797d8ae1ae1887956f1b229206fc4ad7bed6
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-06-05 03:09:13 -0700

    add express server

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,9 @@
-// app.ts
 import express from 'express'
-import productionLogger from './logger'
 
 const app = express()
 
-// middleware
-
-const logger = productionLogger()
-
-app.use(express.json())
-
-// routes
 app.get('/', (req, res) => {
-    logger.info('Root endpoint hit')
-    logger.error('Root endpoint hit')
-    logger.warn('Root endpoint hit')
-    res.send('hello world')
+    res.send('Welcome to auth service')
 })
-
-app.post('/event', () => {})
-app.post('/quests/complete', () => {})
-app.post('hearts/refill', () => {})
-app.get('user/:id/status', () => {})
-app.get('quests/daily', () => {})
-app.get('leaderboard', () => {})
 
 export default app
