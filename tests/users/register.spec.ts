d4acb5e851c0de00cbe450e91e53a41e750ef1ca
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-06-11 10:20:19 -0700

    add email sanitization 'trimming white spaces'

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -214,5 +214,24 @@ describe('POST /auth/register', () => {
             const users = await userRepository.find()
             expect(users).toHaveLength(0)
         })
+        it('should trim the email field `remove white spaces `', async () => {
+            /* Arrange */
+            const userData = {
+                firstName: 'Rakesh',
+                lastName: 'K',
+                email: ' 123@gmail.com ',
+                password: 'secret',
+            }
+
+            /* Act */
+
+            await request(app).post('/auth/register').send(userData)
+
+            /* Assert */
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+            const user = users[0]
+            expect(user.email).toBe('123@gmail.com')
+        })
     })
 })
