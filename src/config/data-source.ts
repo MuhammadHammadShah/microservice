ad25570ee00d61abe3f2ede178c89564bbd90a04
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-08-24 00:36:20 +0500

    add tenants table

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,8 +1,7 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "../entity/User";
+
 import { Config } from ".";
-import { RefreshToken } from "../entity/RefreshToken";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -16,7 +15,7 @@ export const AppDataSource = new DataSource({
     /* Don't use this in production `synchronize` , always keep `false`  */
     synchronize: false,
     logging: false,
-    entities: [User, RefreshToken], // Whenever, We create an entity, we have to register it here.
+    entities: ["src/entity/*.ts"], // Whenever, We create an entity, we have to register it here.
     migrations: ["src/migration/*.ts"],
     subscribers: [],
 });
