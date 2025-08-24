c5436475d600a6f20ab9669f3be2b405d98246f8
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-08-24 15:40:36 +0500

    add create tenant endpoint

--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -2,6 +2,7 @@ import { DataSource } from "typeorm";
 import { AppDataSource } from "../../src/config/data-source";
 import request from "supertest";
 import app from "../../src/app";
+import { Tenant } from "../../src/entity/Tenant";
 
 describe("POST /tenants", () => {
     let connection: DataSource;
@@ -34,6 +35,21 @@ describe("POST /tenants", () => {
                 .send(tenantData);
 
             expect(response.statusCode).toBe(201);
+        });
+        it("should save a tenant in the database", async () => {
+            const tenantData = {
+                name: "Tenat Name",
+                address: "Tenant Address",
+            };
+
+            await request(app).post("/tenants").send(tenantData);
+
+            const tenantRepository = connection.getRepository(Tenant);
+            const tenants = await tenantRepository.find();
+
+            expect(tenants).toHaveLength(1);
+            expect(tenants[0].name).toBe(tenantData.name);
+            expect(tenants[0].address).toBe(tenantData.address);
         });
     });
 });
